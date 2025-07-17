1a5428d19ef13c94e06754530acb120b5e34f17c
Gravy <32828725+Grayyy1219@users.noreply.github.com>
2025-07-17 11:57:21 +0000

    d

--- connection/config.php
+++ connection/config.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 // Database configuration
 if ($_SERVER['HTTP_HOST'] === 'localhost' || $_SERVER['HTTP_HOST'] === '127.0.0.1') {
     define('DB_HOST', '127.0.0.1');
