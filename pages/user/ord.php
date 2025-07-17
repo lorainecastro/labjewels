fd0cd8208d9ea1a9546f8b7554dfb70eb4211257
lorainecastro <lrntrcstr@gmail.com>
2025-07-17 19:11:25 +0800

    commit

--- pages/user/ord.php
+++
@@ -1,2 +0,0 @@
-<div class="item-price">PHP <?php echo number_format($item['price'] * $item['quantity'], 2); ?></div>
-<div class="item-price">PHP <?php echo number_format($order['total_amount'], 2); ?></div>
